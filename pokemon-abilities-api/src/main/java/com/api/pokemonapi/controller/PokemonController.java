57a58467fccb1423b328b6034f52a5b46c6a6126
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-25 18:46:22 -0300

    feat: enable CORS for Pokemon API controller

--- pokemon-abilities-api/src/main/java/com/api/pokemonapi/controller/PokemonController.java
+++ pokemon-abilities-api/src/main/java/com/api/pokemonapi/controller/PokemonController.java
@@ -4,13 +4,11 @@ import com.api.pokemonapi.dtos.responses.PokemonAbilitiesResponseDTO;
 import com.api.pokemonapi.service.PokemonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/pokemon")
 @RequiredArgsConstructor
 public class PokemonController {
