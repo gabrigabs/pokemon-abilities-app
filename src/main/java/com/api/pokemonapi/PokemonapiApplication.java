58a9a870008cfdcafd688a431bd01adcff66a43e
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-25 11:34:02 -0300

    feat: application init

---
+++ src/main/java/com/api/pokemonapi/PokemonapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.pokemonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonapiApplication.class, args);
+	}
+
+}
